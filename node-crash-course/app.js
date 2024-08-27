0756870dcab71f22c84bef9ab36cdcb2738d028b
aimee-annabelle <inezannabelle@gmail.com>
2024-08-27 09:06:50 +0200

    express use

---
+++ node-crash-course/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+//rediect
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+// page not found
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
+});
